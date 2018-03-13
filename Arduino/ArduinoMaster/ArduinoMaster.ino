a4e1bfc2ceb916f855a3bc81d6873b5c3df691ae
CezanvM <ctmeijen@avans.nl>
2018-03-13 16:07:33 +0100

    Arduino cpp & h file
    arduino able to check if data is starting and ending. cpp and h file
    added for parsing

---
+++ Arduino/ArduinoMaster/ArduinoMaster.ino
@@ -0,0 +1,42 @@
+#include "DataParser.h"
+
+int i = 0;
+String Data;
+void setup() {
+
+   Serial.begin(115200);
+  // put your setup code here, to run once:
+  //SerialMeter.begin(115200);
+ 
+
+}
+
+void loop() {
+
+  if(Serial.available())
+  {
+    char c = Serial.read();
+
+    if(c == '\n')
+    {
+      Serial.println("enterfound");
+    }
+    if(c == '/')
+    {
+      Data = "";
+      Serial.println("datastream started");
+    }
+    
+    if(c == '!')
+    {
+      Serial.println("data received");
+      Serial.print(Data);
+     
+    } 
+    Data.concat(c);
+    
+
+  }
+
+  
+}
