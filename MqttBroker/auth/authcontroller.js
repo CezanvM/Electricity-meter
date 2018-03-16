52336aab906bc8b6bddcf38915fb27f070e3429e
Cas de Groot <casdegroot1998@gmail.com>
2018-03-16 17:23:34 +0100

    added enctyption for passwords

--- MqttBroker/auth/authcontroller.js
+++ MqttBroker/auth/authcontroller.js
@@ -1,18 +1,24 @@
 var userModel = require('./usermodel').userModel;
-
+var bcrypt = require('bcrypt');
 exports.auth = function (client, username, password, callback) {
 
     if(username && password) {
         userModel.findOne({
-            name: username,
-            password: password
+            name: username
         }, (err, user) => {
             if(err) throw err;
 
             if(!user)
                 callback(null, false);
-            else
-                callback(null, true);
+
+            if(user) {
+                bcrypt.compare(password.toString(), user.password, (err, isMatch) => {
+
+                    if(err) callback(null, false);
+                    if(!isMatch) callback(null, false);
+                    callback(null, true);
+                });
+            }
         }).catch((err) => {
             if(err) throw err;
         });
