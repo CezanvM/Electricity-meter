bdc4f78d9dcf9e6749b0aa047163ff840a11978c
Cas de Groot <casdegroot1998@gmail.com>
2018-03-17 00:19:23 +0100

    fucked up

---
+++ ElectricityMeterFrontend/src/app/login/services/auth.service.ts
@@ -0,0 +1,59 @@
+import {Injectable, OnDestroy} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/shareReplay';
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/filter';
+import * as moment from 'moment';
+import { User } from '../../data/user/user.class';
+import {Subject} from 'rxjs/Subject';
+
+@Injectable()
+export class AuthService implements  OnDestroy {
+
+  private stop$: Subject<any>;
+
+  constructor(private http: HttpClient) {
+    this.stop$ = new Subject<any>();
+  }
+
+  login(username: string, password: string ) {
+    return this.http.post<any>('authenticate', {name: username, password: password})
+      .shareReplay()
+      .takeUntil(this.stop$)
+      .filter(e => e.id_token !== null && e.expires_at !== null)
+      .do(e => this.setSession);
+  }
+
+  ngOnDestroy(): void {
+    this.stop$.next(true);
+    this.stop$.complete();
+    this.stop$.unsubscribe();
+  }
+
+  private setSession(authResult) {
+
+    localStorage.setItem('id_token', authResult.id_token);
+    localStorage.setItem('expires_at', JSON.stringify(authResult.expires_at) );
+  }
+
+  logout() {
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem('expires_at');
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
+  }
+
+}
