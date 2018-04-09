84a726c7e64b3f744d601bb66e8f0e4d8e7fea0d
Cas de Groot <casdegroot1998@gmail.com>
2018-04-09 09:23:16 +0200

    changed baseurl

--- ElectricityMeterFrontend/src/environments/environment.ts
+++ ElectricityMeterFrontend/src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'http://localhost:2500',
-  // baseUrl: 'http://165.227.180.251:2500'
+  // baseUrl: 'http://localhost:2500',
+  baseUrl: 'http://165.227.180.251:2500'
 };
